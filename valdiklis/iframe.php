3abb784209fe9404bee99bfcf5c752a6d132c5d9
Aurimas R. <mail@aurimas.eu>
2014-08-07 22:16:39 +0200

    - Changed social links to point to current base url  - Changed hardcoded domain name in php files and javascript files

--- valdiklis/iframe.php
+++ valdiklis/iframe.php
@@ -1,14 +1,12 @@
+<?php include 'generator.php'; ?>
 <html>
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-		<link rel="stylesheet" type="text/css" href="http://lplius.lt/valdiklis/style.css?v=2"></link>		
+		<link rel="stylesheet" type="text/css" href="<?php echo Grid::$baseUrl; ?>valdiklis/style.css?v=2"></link>		
 	</head>
 	<body>
 		<div class="lplius-widget-container">
-			<?php
-				include 'generator.php';
-				echo CONTENTS;
-			?>
+			<?php   echo CONTENTS; ?>
 		</div>
 	</body>
 </html>
